bbdd980398e53ce184ebf12d45d67bab6eca49af
JamesHoulee <jmhoule10@gmail.com>
2019-05-13 11:11:30 -0400

    Initial Commit

---
+++ mainMenu.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ * 
+ */
+public class mainMenu extends JFrame{
+
+    /**
+     * 
+     */
+    public mainMenu (){
+        super ("The Anxious Life Of Alex Jo");
+    }
+}
