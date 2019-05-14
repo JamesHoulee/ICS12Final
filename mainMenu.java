a0224b6dab1e77dd87abae5212862e2fbadae258
King48 <juandiego085@gmail.com>
2019-05-14 12:09:02 -0400

    just changed the way JFrame is implemented

--- mainMenu.java
+++ mainMenu.java
@@ -3,19 +3,17 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
- * 
+ * This class is the main menu for our game. 
  */
-public class mainMenu extends JFrame{
+public class MainMenu{
 
     /**
      * 
      */
-    public mainMenu (){
-        super ("The Anxious Life Of Alex Jo");
-
-        setVisible(true);
+    public MainMenu (){
+        JFrame frame = new JFrame ("The Anxious Life Of Alex Jo"); 
+        frame.getContentPane().addMouseListener(new ClickListener())l;        setVisible(true);
         setSize (800,550);
-
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     }
 }
