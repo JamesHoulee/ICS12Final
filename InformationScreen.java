7fa4cdc660cd13579e3d344ae451e946ab814686
King48 <juandiego085@gmail.com>
2019-05-22 11:22:38 -0400

    created this class, drew a string

---
+++ InformationScreen.java
@@ -0,0 +1,24 @@
+import javax.swing.JPanel; 
+
+/**
+ * 
+ * 
+ */
+
+public class InformationScreen extends JPanel{
+
+    CustomButton button; 
+    private String text = "";
+
+    public InformationScreen (){
+        setSize(1000,750); 
+        setVisible (true); 
+        CustomButton = new CustomButton ("Back", 250, 150, 10, 10); 
+    }    
+    public void paintComponent (Graphics g){
+        g.setColor(Color.BLACK); 
+        g.setFont (Font.decode ("arial-BOLD-24")); 
+        g.drawString ("Just Testing", 100,100); 
+    }
+
+}
